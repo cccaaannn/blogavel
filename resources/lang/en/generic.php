173628af3ca704a0e2fcca44dc072163495fb399
cccaaannn <can.kurt.aa@gmail.com>
2024-11-27 00:47:23 +0300

    locale middleware

---
+++ resources/lang/en/generic.php
@@ -0,0 +1,41 @@
+<?php
+return [
+    'english' => 'English',
+    'turkish' => 'Turkish',
+
+    'welcome_home' => 'Welcome to Blogavel',
+    'hello_user' => 'Hello :name!',
+
+    'login' => 'Login',
+    'logout' => 'Logout',
+    'register' => 'Register',
+    'name' => 'Name',
+    'password' => 'Password',
+    'email' => 'Email',
+    'confirm_password' => 'Confirm password',
+
+    'profile' => 'Profile',
+    'home' => 'Home',
+    'posts' => 'Posts',
+    'post' => 'Post',
+    'my_posts' => 'My Posts',
+    'edit_post' => 'Edit Post',
+    'create_post' => 'Create Post',
+    'save' => 'Save',
+    'update' => 'Update',
+    'delete' => 'Delete',
+    'read_more' => 'Read more',
+    'create_post_button' => 'Create Post +',
+    'create_post' => 'Create Post',
+    'title' => 'Title',
+    'content' => 'Content',
+    'blogavel' => 'Blogavel',
+    'blogavel_description' => 'A simple blog application with Laravel',
+
+    'error' => 'Error',
+    'incorrect_credentials' => 'Name or password is incorrect.',
+
+    'avatar_upload_success' => 'Avatar uploaded successfully',
+    'upload_avatar' => 'Upload Avatar',
+    'upload' => 'Upload',
+];
---
+++ resources/lang/tr/generic.php
@@ -0,0 +1,41 @@
+<?php
+return [
+    'english' => 'İngilizce',
+    'turkish' => 'Türkçe',
+
+    'welcome_home' => 'Blogavel\'e hoşgeldiniz',
+    'hello_user' => 'Merhaba :name!',
+
+    'login' => 'Giriş',
+    'logout' => 'Çıkış Yap',
+    'register' => 'Kayıt ol',
+    'name' => 'İsim',
+    'password' => 'Parola',
+    'email' => 'E-posta',
+    'confirm_password' => 'Parola tekrar',
+
+    'profile' => 'Profil',
+    'home' => 'Anasayfa',
+    'posts' => 'Yazılar',
+    'post' => 'Yazı',
+    'my_posts' => 'Yazılarım',
+    'edit_post' => 'Yazıyı Düzenle',
+    'create_post' => 'Yazı Oluştur',
+    'save' => 'Kaydet',
+    'update' => 'Düzenle',
+    'delete' => 'Sil',
+    'read_more' => 'Devamını oku',
+    'create_post_button' => 'Yazı Oluştur +',
+    'create_post' => 'Yazı Oluştur',
+    'title' => 'Başlık',
+    'content' => 'İçerik',
+    'blogavel' => 'Blogavel',
+    'blogavel_description' => 'Laravel ile yapılmış basit bir blog uygulaması',
+
+    'error' => 'Hata',
+    'incorrect_credentials' => 'İsim veya parola yanlış.',
+
+    'avatar_upload_success' => 'Avatar başarıyla yüklendi',
+    'upload_avatar' => 'Avatar Yükle',
+    'upload' => 'Yükle',
+];
