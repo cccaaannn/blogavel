6b71a958797ed2c37350bd0aa9940b858dcb940c
cccaaannn <can.kurt.aa@gmail.com>
2024-11-26 21:26:29 +0300

    error pages

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('title', '404')
+
+@section('layout_content')
+    <div class="w-full h-full flex items-center justify-center text-center">
+        <h1 class="text-7xl font-bold">
+            404
+        </h1>
+    </div>
+@endsection
---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('title', '500')
+
+@section('layout_content')
+    <div class="w-full h-full flex items-center justify-center text-center">
+        <h1 class="text-7xl font-bold">
+            500
+        </h1>
+    </div>
+@endsection
