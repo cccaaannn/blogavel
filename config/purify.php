d96feb9df174c6746454312da26b10c402f7ca42
cccaaannn <can.kurt.aa@gmail.com>
2024-11-28 00:31:12 +0300

    config

---
+++ config/purify.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'configs' => [
+        'custom' => [
+            'HTML.Allowed' => 'h1,h2,h3,h4,h5,h6,b,u,strong,i,em,s,del,a[href|title],ul,ol,li,p[style],div[style],span[style],br,span,img[width|height|alt|src],blockquote',
+            'CSS.AllowedProperties' => 'font,font-size,font-weight,font-style,font-family,text-decoration,padding-left,color,background-color,text-align',
+        ],
+    ]
+];
